b929adfe24f2fb100089666b1b5913221d4bda9e
jsepucas <jsepucas@myuax.com>
2024-04-15 19:03:44 +0200

    Commitss

--- src/main/java/A_AnalisisGenomico/ConteoGenesGUI.java
+++ src/main/java/A_AnalisisGenomico/ConteoGenesGUI.java
@@ -1,10 +1,19 @@
 package A_AnalisisGenomico;
 
-public class ConteoGenesGUI extends JFrame {
+import javax.swing.*;
+import java.awt.event.*;
 
+public class ConteoGenesGUI extends JFrame {
     private JTextField inputField;
     private JButton countButton;
     private JLabel resultLabel;
 
-    public ConteoGenesGUI()
-}
+    public ConteoGenesGUI() {
+        super("Conteo de Genes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 120);
+        setLayout(new FlowLayout());
+
+        inputField = new JTextField(20);
+        countButton = new JButton("Contar Genes");
+        resultLabel = new JLabel("Resultado aparecerá aquí");
