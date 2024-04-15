cea6c838bbf27d6953a89349527540c5189bec4b
jsepucas <jsepucas@myuax.com>
2024-04-15 19:04:27 +0200

    Commitss

--- src/main/java/A_AnalisisGenomico/ConteoGenesGUI.java
+++ src/main/java/A_AnalisisGenomico/ConteoGenesGUI.java
@@ -17,3 +17,18 @@ public class ConteoGenesGUI extends JFrame {
         inputField = new JTextField(20);
         countButton = new JButton("Contar Genes");
         resultLabel = new JLabel("Resultado aparecerá aquí");
+
+        countButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String dna = inputField.getText();
+                int result = ConteoGenes.contarGenes(dna);
+                resultLabel.setText("Número de genes: " + result);
+            }
+        });
+
+        add(inputField);
+        add(countButton);
+        add(resultLabel);
+
+        setVisible(true);
+    }
