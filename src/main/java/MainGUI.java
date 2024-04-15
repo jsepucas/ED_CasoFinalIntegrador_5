1d4ca883ec302092d4b5f19a1479011c7b8b8bec
jsepucas <jsepucas@myuax.com>
2024-04-15 20:11:05 +0200

    Commitss

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -8,3 +8,28 @@ public class MainGUI extends JFrame {
     private JButton btnConteoGenes;
     private JButton btnCalculoCombinaciones;
 
+    public MainGUI() {
+        super("Sistema de Análisis Genómico de la UAX \uD83D\uDE00 ");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 300);
+        setLayout(new FlowLayout());
+
+        btnConteoGenes = new JButton("Conteo de Genes");
+        btnCalculoCombinaciones = new JButton("Cálculo de Combinaciones");
+
+        btnConteoGenes.addActionListener(e -> new ConteoGenesGUI());
+        btnCalculoCombinaciones.addActionListener(e -> new CalculoDeCombinacionesGUI());
+
+        add(btnConteoGenes);
+        add(btnCalculoCombinaciones);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainGUI();
+    }
+}
+
+
