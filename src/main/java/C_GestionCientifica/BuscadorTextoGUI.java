659e2b9a637c539f0a14feeb260112acf57262d6
jsepucas <jsepucas@myuax.com>
2024-04-15 20:37:01 +0200

    Commitsss

---
+++ src/main/java/C_GestionCientifica/BuscadorTextoGUI.java
@@ -0,0 +1,11 @@
+package C_GestionCientifica;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BuscadorTextoGUI {
+}
