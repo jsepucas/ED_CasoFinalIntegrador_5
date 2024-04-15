bced6c205cf4f2e4331deba32a06e9a1974accff
jsepucas <jsepucas@myuax.com>
2024-04-15 20:32:17 +0200

    Commitsss

--- src/main/java/C_GestionCientifica/OrganizadorDocumentos.java
+++ src/main/java/C_GestionCientifica/OrganizadorDocumentos.java
@@ -17,7 +17,4 @@ public class OrganizadorDocumentos {
         Files.write(file, lineas);
     }
 
-
-
-
 }
---
+++ src/main/java/C_GestionCientifica/OrganizadorDocumentosGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+
+public class OrganizadorDocumentosGUI extends JFrame {
+    private JTextArea textArea;
+    private JButton loadButton, saveButton, sortButton;
+    private JFileChooser fileChooser;
