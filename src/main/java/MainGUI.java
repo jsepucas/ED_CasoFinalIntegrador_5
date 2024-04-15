4846f51978d2d62cf7bd64994cf74718229e2e49
jsepucas <jsepucas@myuax.com>
2024-04-15 20:10:53 +0200

    Commitss

---
+++ src/main/java/MainGUI.java
@@ -0,0 +1,10 @@
+import A_AnalisisGenomico.CalculoDeCombinacionesGUI;
+import A_AnalisisGenomico.ConteoGenesGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainGUI extends JFrame {
+    private JButton btnConteoGenes;
+    private JButton btnCalculoCombinaciones;
+
