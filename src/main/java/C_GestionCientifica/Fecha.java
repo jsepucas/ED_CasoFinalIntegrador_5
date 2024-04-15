3a73e1d65d28cce45ae9332e0d8c6ac3ca49e055
jsepucas <jsepucas@myuax.com>
2024-04-15 21:22:26 +0200

    Commitss

--- src/main/java/C_GestionCientifica/Fecha.java
+++ src/main/java/C_GestionCientifica/Fecha.java
@@ -11,3 +11,25 @@ public class Fecha {
         this.fecha = fecha;
         this.descripcion = descripcion;
     }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Método para comparar fechas, útil para ordenar
+    public static int compararPorFecha(Fecha f1, Fecha f2) {
+        return f1.getFecha().compareTo(f2.getFecha());
+    }
+}
