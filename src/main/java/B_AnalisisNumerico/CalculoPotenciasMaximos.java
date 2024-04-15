098ae0acf71974b97aa5823daf8d37c1d61b83da
jsepucas <jsepucas@myuax.com>
2024-04-15 19:54:16 +0200

    Commitss

---
+++ src/main/java/B_AnalisisNumerico/CalculoPotenciasMaximos.java
@@ -0,0 +1,12 @@
+package B_AnalisisNumerico;
+
+public class CalculoPotenciasMaximos {
+
+    public static int calcularPotencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * calcularPotencia(base, exponente - 1);
+        }
+    }
+}
