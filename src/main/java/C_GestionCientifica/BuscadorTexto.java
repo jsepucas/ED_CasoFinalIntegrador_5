379246cf9a1ccf1d657c3a31d34889769783a83f
jsepucas <jsepucas@myuax.com>
2024-04-15 20:36:13 +0200

    Commitsss

---
+++ src/main/java/C_GestionCientifica/BuscadorTexto.java
@@ -0,0 +1,10 @@
+package C_GestionCientifica;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class BuscadorTexto {
+}
