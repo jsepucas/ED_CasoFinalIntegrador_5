702c8b72d96df5834917eac260a987549adf23b9
jsepucas <jsepucas@myuax.com>
2024-04-15 18:59:52 +0200

    GUI

---
+++ src/main/java/A_AnalisisGenomico/ConteoGenesGUI.java
@@ -0,0 +1,10 @@
+package A_AnalisisGenomico;
+
+public class ConteoGenesGUI extends JFrame {
+
+    private JTextField inputField;
+    private JButton countButton;
+    private JLabel resultLabel;
+
+    public ConteoGenesGUI()
+}
