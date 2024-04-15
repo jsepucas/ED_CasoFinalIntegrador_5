7dea3a8006c87bff55f78e3c22f8bd49d648d1d9
jsepucas <jsepucas@myuax.com>
2024-04-15 20:32:24 +0200

    Commitsss

--- src/main/java/C_GestionCientifica/OrganizadorDocumentosGUI.java
+++ src/main/java/C_GestionCientifica/OrganizadorDocumentosGUI.java
@@ -7,3 +7,9 @@ public class OrganizadorDocumentosGUI extends JFrame {
     private JTextArea textArea;
     private JButton loadButton, saveButton, sortButton;
     private JFileChooser fileChooser;
+
+    public OrganizadorDocumentosGUI() {
+        super("Organizador de Documentos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 300);
+        setLayout(new BorderLayout());
