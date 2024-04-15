f0b3ab68aaeec4660d47cae165834d4374c8fd54
jsepucas <jsepucas@myuax.com>
2024-04-15 19:58:54 +0200

    Commitss

---
+++ src/main/java/B_AnalisisNumerico/SumatorioListadoGUI.java
@@ -0,0 +1,15 @@
+package B_AnalisisNumerico;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class SumatoriaListadoGUI extends JFrame {
+    private JTextField inputFieldSumatoria;
+    private JButton calculateSumButton;
+    private JLabel sumResultLabel;
+    private JTextField inputFieldDesde, inputFieldHasta;
+    private JButton listNumbersButton;
+    private JTextArea listResultArea;
