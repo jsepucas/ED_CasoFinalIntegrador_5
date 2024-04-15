8e06a397d6414264c5261c994c024e7d54210ef8
jsepucas <jsepucas@myuax.com>
2024-04-15 19:23:57 +0200

    Commitss

--- src/main/java/A_AnalisisGenomico/CalculoDeCombinacionesGUI.java
+++ src/main/java/A_AnalisisGenomico/CalculoDeCombinacionesGUI.java
@@ -6,3 +6,9 @@ public class CalculoDeCombinacionesGUI extends JFrame {
     private JTextField inputField;
     private JButton generateButton;
     private JTextArea resultArea;
+
+    public CalculoDeCombinacionesGUI() {
+        super("Calculo de Combinaciones Genéticas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(350, 300);
+        setLayout(new FlowLayout());
