6572c2c238bbb09dadf5611e2c03451765bcc911
jsepucas <jsepucas@myuax.com>
2024-04-15 20:36:28 +0200

    Commitsss

--- src/main/java/C_GestionCientifica/BuscadorTexto.java
+++ src/main/java/C_GestionCientifica/BuscadorTexto.java
@@ -7,4 +7,16 @@ import java.nio.file.Paths;
 import java.io.IOException;
 
 public class BuscadorTexto {
+
+    public static List<Integer> buscarPalabra(String pathToFile, String palabra) throws IOException {
+        List<String> lineas = Files.readAllLines(Paths.get(pathToFile));
+        List<Integer> lineasEncontradas = new ArrayList<>();
+        for (int i = 0; i < lineas.size(); i++) {
+            if (lineas.get(i).contains(palabra)) {
+                lineasEncontradas.add(i + 1);  // Sumamos 1 para que la numeración de línea sea más humana
+            }
+        }
+        return lineasEncontradas;
+    }
+
 }
