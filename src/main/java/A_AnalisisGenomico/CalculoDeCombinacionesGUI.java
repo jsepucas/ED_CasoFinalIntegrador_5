c21290a6774345273b8fa6e62ad0b17e4badebd3
jsepucas <jsepucas@myuax.com>
2024-04-15 19:23:50 +0200

    Commitss

---
+++ src/main/java/A_AnalisisGenomico/CalculoDeCombinacionesGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.List;
+
+public class CalculoDeCombinacionesGUI extends JFrame {
+    private JTextField inputField;
+    private JButton generateButton;
+    private JTextArea resultArea;
