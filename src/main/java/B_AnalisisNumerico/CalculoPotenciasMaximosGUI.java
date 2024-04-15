2811a8d420066d3febea4132cc30c94b02423c31
jsepucas <jsepucas@myuax.com>
2024-04-15 20:02:02 +0200

    Commitss

--- src/main/java/B_AnalisisNumerico/CalculoPotenciasMaximosGUI.java
+++ src/main/java/B_AnalisisNumerico/CalculoPotenciasMaximosGUI.java
@@ -6,4 +6,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculoPotenciasMaximosGUI {
+
+    private JTextField baseField, exponenteField, arrayField;
+    private JButton calculatePowerButton, findMaxButton;
+    private JLabel powerResultLabel, maxResultLabel;
+
+    public CalculoPotenciasMaximosGUI() {
+        super("Cálculo de Potencias y Máximos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 200);
+        setLayout(new FlowLayout());
 }
