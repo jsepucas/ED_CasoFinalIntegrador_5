c90ddc56f496d082735b5b918f0fc94b5a7fe546
jsepucas <jsepucas@myuax.com>
2024-04-15 19:14:35 +0200

    Commitss

--- src/main/java/A_AnalisisGenomico/CalculoDeCombinaciones.java
+++ src/main/java/A_AnalisisGenomico/CalculoDeCombinaciones.java
@@ -2,3 +2,12 @@ package A_AnalisisGenomico;
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class CalculoDeCombinaciones {
+
+    public static List<String> generarCombinaciones(int n) {
+        String[] bases = {"A", "C", "G", "T"};
+        List<String> result = new ArrayList<>();
+        generarCombinacionesRecursivo("", bases, n, result);
+        return result;
+    }
